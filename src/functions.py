2bf4a5ef46bfbf9644450cf0c8bbc1a1bbf3662c
Vanessa Villalba Simon <vvillalba@wpi.edu>
2026-02-08 15:06:18 -0500

    Changed version packages in requirements.txt, edited sea ice data, added functions to functions.py, and added main in sonicweather.py

---
+++ src/functions.py
@@ -0,0 +1,16 @@
+import pandas as pd
+import numpy as np
+import pretty_midi
+import matplotlib.pyplot as plt
+
+def loadAllData():
+    return
+
+def preProcessData():
+    return
+
+def normalizeData():
+    return
+
+def generatePiece():
+    return
